c60a95f68067565f874140918b4ac3a95b911a8c
MohanRamaera <mohan.it@ramaera.in>
2022-12-29 10:12:45 +0530

    added document upload

--- src/medias/medias.module.ts
+++ src/medias/medias.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'nestjs-prisma';
 import { MediasController } from './medias.controller';
-import { MediaService } from './medias.service';
+
 
 @Module({
   controllers: [MediasController],
-  providers:[MediaService],
+  providers:[],
   imports:[PrismaModule]
 
 })
