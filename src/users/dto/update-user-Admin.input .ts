2d882dbe7f7c93b6528de8b3733e1b076cf69f46
MohanRamaera <mohan.it@ramaera.in>
2023-04-08 11:51:22 +0530

    UpdateuserbyAdmin Mutation updated for nominee & DOcumnets

--- src/users/dto/update-user-Admin.input .ts
+++ src/users/dto/update-user-Admin.input .ts
@@ -1,5 +1,7 @@
 import { InputType, Field, registerEnumType } from '@nestjs/graphql';
 import { KYC, Membership } from '@prisma/client';
+import { NomineeInput } from './createNominee.input';
+import { UpdateDocumentsInput } from 'src/documents/dto/update-document';
 
 
 
@@ -12,37 +14,53 @@ export class UpdateUserInputByAdmin {
 
   @Field()
   id:string
-
-
-  @Field({ nullable: true })
-  name?: string;
-
-  @Field({ nullable: true })
-  father_or_husband_name?: string;
-
-
-  @Field({ nullable: true })
-  email?: string;
-
-
-   @Field({ nullable: true })
-  mobile_number?: string
-
-   @Field({ nullable: true })
-  alternate_mobile_number?: string
-
-
-   @Field({ nullable: true })
-  date_of_birth?: string
-
-   @Field({ nullable: true })
-  demat_account?: string
-
   
-  @Field(()=>KYC)
-  kyc?: KYC;
+    @Field({ nullable: true })
+    name?: string;
+  
+    @Field({ nullable: true })
+    father_or_husband_name?: string;
+  
+  
+    @Field({ nullable: true })
+    email?: string;
+  
+  
+     @Field({ nullable: true })
+    mobile_number?: string
+  
+     @Field({ nullable: true })
+    alternate_mobile_number?: string
+  
+  
+     @Field({ nullable: true })
+    date_of_birth?: string
+  
+     @Field({ nullable: true })
+    demat_account?: string
+  
+    @Field(()=>KYC)
+    kyc?: KYC;
 
-}
+    @Field(()=>[NomineeInput])
+    nominee?: NomineeInput[];
+
+    @Field(()=>[UpdateDocumentsInput])
+    document?:UpdateDocumentsInput[]
+  
+  }
+
+  // @InputType()
+  // export class NomineeInputs {
+  //   @Field({ nullable: false })
+  //   id: string;
+  
+  //   @Field(()=>String)
+  //   name: string;
+  
+  //   @Field(()=>String)
+  //   relationship: string;
+  // }
 
 
 
