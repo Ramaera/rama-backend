991f0504980b06de8095928b270f50bb4f110e8f
MohanRamaera <mohan.it@ramaera.in>
2023-05-13 08:56:34 +0530

    increased the file upload size

--- src/medias/medias.controller.ts
+++ src/medias/medias.controller.ts
@@ -15,7 +15,7 @@ import { join } from 'path';
 import Express from 'express';
 
 import 'multer';
- 
+
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { extname } from 'path';
 import { diskStorage } from 'multer';
@@ -23,18 +23,19 @@ import { ApiBody, ApiTags } from '@nestjs/swagger';
 
 export const editFileName = (req, file, callback) => {
   const fileExtName = extname(file.originalname);
- 
+
   // const randomName = `${(Math.random() + 1)
   //     .toString(36)
   //     .substring(7)
   //     .toLocaleUpperCase()}`;
 
-  const randomName =     Date.now().toString(36) + Math.random().toString(36).substring(2);
+  const randomName =
+    Date.now().toString(36) + Math.random().toString(36).substring(2);
   callback(null, `${randomName}${fileExtName}`);
 };
 
 export const imageFileFilter = (req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|jfif)$/)) {
+  if (!file.originalname.match(/\.(jpg|jpeg|jpg|png|jfif)$/)) {
     return callback(new Error('Only image files are allowed!'), false);
   }
   callback(null, true);
@@ -51,36 +52,41 @@ export class MediasController {
         filename: editFileName,
       }),
       fileFilter: imageFileFilter,
+      limits: {
+        fileSize: 1024 * 1024 * 10, // 10MB file size limit
+      },
     })
   )
   @ApiBody({
-    schema:{
-      type:'object',
-      properties:{
-        photo:{
-          type:'string',
-          format:'binary',
-        }
-      }
-    }
+    schema: {
+      type: 'object',
+      properties: {
+        photo: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
   })
-  uploadSingle(@UploadedFile() file,@Headers() header) {
-    const  origin =header.host
+  uploadSingle(@UploadedFile() file, @Headers() header) {
+    const origin = header.host;
 
     const url = `http://${origin}/documents/${file.filename}`;
-   
+
     const response = {
       originalname: file.originalname,
       filename: file.filename,
-      url
+      url,
     };
 
-    console.log({response})
+    console.log({ response });
     return response;
   }
-  @Get(":name")
+  @Get(':name')
   getFile(@Param() params): StreamableFile {
-    const file = createReadStream(join(process.cwd(), `uploads/${params.name}`));
+    const file = createReadStream(
+      join(process.cwd(), `uploads/${params.name}`)
+    );
     return new StreamableFile(file);
   }
 }
